63f06d8bc74318b12a0fde0c58efcc34bcafc91e
webedge <jon.harding@webedgemedia.com>
2014-06-05 14:34:52 -0500

    finalizing demo page

--- demo/JS/menu.js
+++ demo/JS/menu.js
@@ -2,4 +2,12 @@
     $('.navbar-toggle').on('click', function () {
         $('.collapse, #mainContainer').toggleClass('in');
     });
+
+    
+});
+
+$(window).resize(function () {
+    if ($(window).width() > 768) {
+        $('.collapse, #mainContainer').removeClass('in');
+    };
 });
